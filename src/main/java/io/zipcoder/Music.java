22110922fd20acd7a970be2a8bede57132b578ef
randy <randyr_1993@yahoo.com>
2022-09-12 00:03:43 -0400

    Completed Lab, All Tests Pass

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
 
     private String[] playList;
@@ -8,7 +11,33 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int forwardMethod = playList.length, backwardMethod = playList.length, temp = 0;
+        List<Integer> endingIndex = new ArrayList<Integer>();
+        //for multiple copies of q
+        for (int i = 0; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                endingIndex.add(i);
+            }
+        }
+        for (Integer s : endingIndex) {
+            if (s > startIndex) {
+                temp = s - startIndex;
+                if (temp < forwardMethod)
+                    forwardMethod = temp;
+                temp = startIndex + (playList.length - s);
+                if (temp < backwardMethod)
+                    backwardMethod = temp;
+            }
+            if (s < startIndex) {
+                temp = (playList.length - startIndex) + s;
+                if (temp < forwardMethod)
+                    forwardMethod = temp;
+                temp = startIndex - s;
+                if (temp < backwardMethod)
+                    backwardMethod = temp;
+            }
+        }
+        return forwardMethod < backwardMethod ? forwardMethod : backwardMethod;
     }
 }
